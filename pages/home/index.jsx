109e344e47e38fe7652a23c46951be287a990ade
sarpergulfirat <sarpergulfirat@gmail.com>
2023-03-16 18:55:28 +0300

    react formik and yup validation completed

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -4,16 +4,18 @@ import React from 'react'
 import MenuWrapper from "@/components/products/MenuWrapper";
 import About from "@/components/About";
 import Reservation from "@/components/Reservation";
+import Customers from "@/components/customers/Customers";
 
 const Index = () => {
   return (
-    <div>
+    <React.Fragment>
       <Carousel />
       <Campaigns />
       <MenuWrapper/>
       <About />
       <Reservation />
-    </div>
+      <Customers />
+    </React.Fragment>
   )
 }
 
